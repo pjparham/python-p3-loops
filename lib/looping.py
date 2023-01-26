988c050e209d461b158f2026b795cfd664327bfe
Parker Parham <parkerparham@gmail.com>
2023-01-26 15:50:47 -0500

    Done with assignment

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here
-    pass
+    for i in range(10):
+        print(f"{10 - i}")
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here
-    pass
+    square_list = [number * number for number in int_list]
+    return square_list
 
 def fizzbuzz():
-    # code goes here
-    pass
+    for i in range(101):
+        if i == 0:
+            pass
+        elif i % 5 == 0 and i % 3 == 0:
+            print("FizzBuzz")
+        elif i % 5 == 0:
+            print("Buzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        else:
+            print(i)
+
